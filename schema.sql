7cd5aaa2c6d83b3ea5346ad3a8e583aaf8706e40
Shayna Jefferson <shaynajj@gmail.com>
2020-08-31 20:09:23 -0400

    adding remaining files from module

--- schema.sql
+++ schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE employees (
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
 );
+--DROP TABLE dept_manager;
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -31,21 +32,29 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+--DROP TABLE dept_emp;
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
   dept_no VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
 );
+--DROP TABLE titles;
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-SELECT * FROM departments;
+  
+--PRIMARY KEY (emp_no, title)
+--SELECT * FROM departments;
+--SELECT * FROM employees;
+--SELECT * FROM dept_emp;
+--SELECT * FROM dept_manager;
+--SELECT * FROM salaries;
+--SELECT * FROM titles;
